e0f417459e0d1deb0f10e77172c86c57bc2e3f06
sass846 <skiran.self@gmail.com>
2024-02-18 10:48:27 +0530

    added dwm-autostart

---
+++ .dwm/autostart.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Set wallpaper using feh
+feh --bg-fill /home/stryder/Wallpaper/Wallpaper.png &
+
+# Start picom for compositing (transparency and shadows)
+picom &
+
+# Start slstatus
+# slstatus &
+
+# Start  dunst
+dunst &
+
+#Turn off screen blanking
+xset s off -dpms
